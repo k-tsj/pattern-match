14feabe56b6784c66b89adae110215338ad12a8d
Kazuki Tsujimoto <kazuki@callcc.net>
2012-03-19 00:34:39 +0900

    Add a reference to a pattern variable

--- benchmark/bm_match.rb
+++ benchmark/bm_match.rb
@@ -4,7 +4,7 @@ require 'pattern-match'
   match([[0, [1, 2], [3, 4]], [5, [6, 7], [8, 9]], [10, [11, 12], [13, 14]]]) {
     with(_[a, _]) { :not_match }
     with(_[a, _]) { :not_match }
-    with(_[_[a, _[b, ___], ___], ___]) { }
+    with(_[_[a, _[b, ___], ___], ___]) { a }
     with(_[a, _]) { :not_match }
     with(_[a, _]) { :not_match }
   }
