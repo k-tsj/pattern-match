f9621f2e03bd8a1db83a4756b4c965ca69586dde
Kazuki Tsujimoto <kazuki@callcc.net>
2014-01-02 12:30:01 +0900

    Update copyright notices for 2014

--- lib/pattern-match.rb
+++ lib/pattern-match.rb
@@ -1,6 +1,6 @@
 # pattern-match.rb
 #
-# Copyright (C) 2012-2013 Kazuki Tsujimoto, All rights reserved.
+# Copyright (C) 2012-2014 Kazuki Tsujimoto, All rights reserved.
 
 require 'pattern-match/version'
 require 'pattern-match/core'
