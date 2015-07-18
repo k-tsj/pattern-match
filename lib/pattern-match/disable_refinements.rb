fe82fa829b958ade59af0cce5197d5fc92071ccf
Kazuki Tsujimoto <kazuki@callcc.net>
2015-07-19 00:44:58 +0900

    Give an original method a unique name

--- lib/pattern-match/disable_refinements.rb
+++ lib/pattern-match/disable_refinements.rb
@@ -1,10 +1,12 @@
 class Module
+  refine_orig = "__refine_orig_#{Time.now.to_i}".to_sym
+
   methods = instance_methods(false) + private_instance_methods(false)
   if methods.include?(:refine)
-    if methods.include?(:__refine_orig)
+    if methods.include?(refine_orig)
       raise LoadError, "can't re-define Module#refine"
     end
-    alias_method :__refine_orig, :refine
+    alias_method refine_orig, :refine
     remove_method :refine
   end
 
@@ -17,9 +19,9 @@ class Module
   ensure
     remove_method :refine
 
-    if Kernel.respond_to?(:__refine_orig, true)
-      alias_method :refine, :__refine_orig
-      remove_method :__refine_orig
+    if Kernel.respond_to?(refine_orig, true)
+      alias_method :refine, refine_orig
+      remove_method refine_orig
     end
   end
 end
